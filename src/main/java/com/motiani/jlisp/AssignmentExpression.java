30d7ad77532bec5a884722c96ec4cc999fce5000
Nitin Motiani <nitin.motiani@gmail.com>
2017-04-25 23:30:08 +0530

    add expression types for assignment and definition

---
+++ src/main/java/com/motiani/jlisp/AssignmentExpression.java
@@ -0,0 +1,14 @@
+package com.motiani.jlisp;
+
+class AssignmentExpression extends Expression {
+
+	private String variable;
+	private Expression expression;
+
+	Expression evaluate(Scope scope) {
+		scope.modify(variable, expression.evaluate(scope));
+		// TODO: Change this to make it consistent with the scheme REPL. Though
+		// it maybe useless anyway
+		return new VariableExpression(variable);
+	}
+}
---
+++ src/main/java/com/motiani/jlisp/DefinitionExpression.java
@@ -0,0 +1,18 @@
+package com.motiani.jlisp;
+
+class DefinitionExpression extends ListExpression {
+	private String variable;
+	private Expression expression;
+
+	public DefinitionExpression(String variable, Expression expression) {
+		this.variable = variable;
+		this.expression = expression;
+	}
+
+	Expression evaluate(Scope scope) {
+		scope.create(variable, expression.evaluate(scope));
+		// TODO: Figure this out. Currently doing this in accordance with what
+		// the scheme REPL shows me
+		return new VariableExpression(variable);
+	}
+}
